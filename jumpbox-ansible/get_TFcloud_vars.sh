92f8bb5bdd9f038403e6749489712749ce7a05b3
ue-joe <89872491+ue-joe@users.noreply.github.com>
2021-09-07 16:14:14 -0400

    Update get_TFcloud_vars.sh

--- jumpbox-ansible/get_TFcloud_vars.sh
+++ jumpbox-ansible/get_TFcloud_vars.sh
@@ -1,3 +1,4 @@
+#takes as input the name of the output var in TF
 export BASE_URL="https://app.terraform.io/api/v2"
 export TOKEN=$TF_API_TOKEN
 export WORKSPACE_ID="ws-vjsSZ83EY1bfvSVh"
@@ -5,7 +6,7 @@ curl -s \
     --header "Authorization: Bearer ${TOKEN}" \
     --header "Content-Type: application/vnd.api+json" \
     --request GET \
-    "${BASE_URL}/workspaces/${WORKSPACE_ID}/current-state-version?include=outputs" | jq 
+    "${BASE_URL}/workspaces/${WORKSPACE_ID}/current-state-version?include=outputs" | jq -r ".included[].attributes | select(.name | contains(\"$1\")).value"
 
 ## now filter me with this: 
 #sh get_TFcloud_vars.sh | jq -r ".included[].attributes | select(.name | contains(\"vpc_id\")).value"
